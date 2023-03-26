0d1451363e4bb3cde5ab7afcf18dd98f46f8b95f
Koh Vinleon <kohvinleon@gmail.com>
2023-03-26 23:45:05 +0800

    Fix routes

--- src/routes/api/cafe.ts
+++ src/routes/api/cafe.ts
@@ -4,21 +4,9 @@ import makeValidator from "../../middlewares/validator-middleware";
 import { getCafeRules, getCafesPaginatedRules } from "../../controllers/api/cafe/validator";
 import { getCafeController, getCafesPaginatedController } from "../../controllers/api/cafe";
 
-import tokenValidatorMiddleware from "../../middlewares/token-validator-middleware";
-
 const cafeRouter = express.Router();
 
-cafeRouter.get(
-  "/",
-  makeValidator(getCafesPaginatedRules),
-  tokenValidatorMiddleware,
-  makeExpressCallback(getCafesPaginatedController),
-);
-cafeRouter.get(
-  "/:cafe_id",
-  makeValidator(getCafeRules),
-  tokenValidatorMiddleware,
-  makeExpressCallback(getCafeController),
-);
+cafeRouter.get("/", makeValidator(getCafesPaginatedRules), makeExpressCallback(getCafesPaginatedController));
+cafeRouter.get("/:cafe_id", makeValidator(getCafeRules), makeExpressCallback(getCafeController));
 
 export default cafeRouter;
