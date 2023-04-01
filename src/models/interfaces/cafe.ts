c060649e6076ab16326fa637f2a5afbf9ed603be
Koh Vinleon <kohvinleon@gmail.com>
2023-04-01 11:29:33 +0800

    Update cafe schema

--- src/models/interfaces/cafe.ts
+++ src/models/interfaces/cafe.ts
@@ -6,15 +6,18 @@ export default interface ICafe {
   address: string;
   open_at: string;
   close_at: string;
+  credit: number;
+  has_wifi: boolean;
+  has_charging: boolean;
+  has_ambience: boolean;
+  image_url: string;
   availability_time_slots: {
     date: string;
     time: string[];
-    timezone: string;
     seat: number;
   }[];
   manager: IUser;
-  credit: number;
-  image_url: string;
+  rating: number;
   deleted_at?: Date;
   created_at: Date;
   updated_at: Date;
--- src/models/schemas/cafe.ts
+++ src/models/schemas/cafe.ts
@@ -1,22 +1,29 @@
+import _ from "lodash";
+import moment from "moment-timezone";
 import mongoose from "mongoose";
+import mongooseLeanVirtuals from "mongoose-lean-virtuals";
 
 const cafeSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     address: { type: String, required: true },
-    open_at: { type: String, required: true },
-    close_at: { type: String, required: true },
+    // TODO: Change if only there's request to support multiple opening hours
+    open_at: { type: String, required: true }, // Assuming that the cafe opens at same time everyday
+    close_at: { type: String, required: true }, // Assuming that the cafe closes at same time everyday
+    credit: { type: Number, default: 0 },
+    has_wifi: { type: Boolean, default: false },
+    has_charging: { type: Boolean, default: false },
+    has_ambience: { type: Boolean, default: false },
+    image_url: { type: String },
     availability_time_slots: [
       {
         date: { type: String, trim: true, default: null },
         time: [{ type: String, trim: true, default: [] }],
-        timezone: { type: String, trim: true, default: "Asia/Singapore" },
         seat: { type: Number },
       },
     ],
     manager: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    credit: { type: Number, default: 0 },
-    image_url: { type: String },
+    rating: { type: Number, default: 0 }, // TODO: Implement rating schema, might change to virtual getter instead
     deleted_at: { type: Date },
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now },
@@ -29,5 +36,17 @@ const cafeSchema = new mongoose.Schema(
     autoCreate: true,
   },
 );
+
+/* It's a virtual getter that returns true if the cafe is open, false otherwise. */
+cafeSchema.virtual("is_open").get(function () {
+  const open_at = _.get(this, "open_at");
+  const close_at = _.get(this, "close_at");
+  const open_at_moment = moment.tz(open_at, "HH:mm", "Asia/Singapore");
+  const close_at_moment = moment.tz(close_at, "HH:mm", "Asia/Singapore");
+  const now = moment.tz("Asia/Singapore");
+  return now.isBetween(close_at_moment, open_at_moment);
+});
+
+cafeSchema.plugin(mongooseLeanVirtuals);
 
 export default cafeSchema;
